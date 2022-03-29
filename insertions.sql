057fa2efc9b6e21bf8b39ff9d5b39f8aa30a0fe6
RCW2000 <70165226+RCW2000@users.noreply.github.com>
2022-03-29 01:35:41 -0400

    Update insertions.sql

--- insertions.sql
+++ insertions.sql
@@ -7,10 +7,14 @@ INSERT INTO  LOCATION VALUES("Entrance", 0 , 0);
 INSERT INTO STAFF VALUES("Salvador",912914404,1,1,15000);
 INSERT INTO STAFF VALUES("Lazarus", 912904770,1,0, 20000);
 INSERT INTO STAFF VALUES("Goldsmith",912904889,1,0,17000);
---exhibit
+-- exhibit
 INSERT INTO EXHIBIT VALUES("Bi-Peds",3,912914404,3,1,"North America");
 INSERT INTO EXHIBIT VALUES("Winged Giants",5,912904889,6,1,"Asia");
---species
+-- species
 INSERT INTO  SPECIES VALUES("Bigfoot", "Bipedius Harriest", 1);
 INSERT INTO  SPECIES VALUES("Mothman", "Flightius Motheus",0);
 INSERT INTO  SPECIES VALUES("Chupacabra", "Vampiricus Felinoness", 0);
+-- animal
+INSERT INTO ANIMAL VALUES("Max", 01295, "Bi-peds", "Bipedius Harriest");
+INSERT INTO ANIMAL VALUES("Buddy", 75688, "Winged Giants", "Flightius Motheus");
+INSERT INTO ANIMAL VALUES("Milo",79863,"Quadrupeds", "Vampiricus Felinoness");
